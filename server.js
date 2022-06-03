b7b07920401dcc03e9d69a8d8bc1f34ae4312327
Moises Silva <mogosilva@gmail.com>
2022-06-02 21:04:04 -0400

    Creating first pass of Rappers API

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require(`express`)
+const app = express()
+const PORT = 8000
+
+const rappers ={ 
+	'21 savage': {
+
+		'birthName': 'Sheyaa Bin Abraham-Joseph',
+		'birthLocation' : 'London, England',
+		'age' : 29,
+	},
+	'chance the rapper': {
+		'birthName': 'Chancelor Bennett',
+		'birthLocation' : 'Chicago, Illinois',
+		'age' : 29,
+	},
+	'unknown' : {
+		'birthName': 'unknown',
+		'birthLocation' : 'unknown',
+		'age' : 'unknown',
+	}
+}
+
+app.get('/', (request, response) => {
+	response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+	const rapperName = request.params.name.toLocaleLowerCase()
+	response.json( rappers[rapperName] ? rappers[rapperName] : rappers['unknown'] )
+})
+
+app.listen(PORT, () => {
+	console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+})
